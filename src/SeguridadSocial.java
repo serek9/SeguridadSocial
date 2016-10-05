79ee54c3bb6f0483e58bc010c73b928f5618d97a
DAM <sergiodiaz808@gmail.com>
2016-10-05 21:10:30 +0200

    HASHMAP PART

--- src/SeguridadSocial.java
+++ src/SeguridadSocial.java
@@ -9,96 +9,41 @@ import java.util.stream.Collectors;
 public class SeguridadSocial {
     private List<Persona> personasList;
 
-    /////////////////////////HASHMAP PART:
+    public SeguridadSocial() {
+        personasList = new ArrayList<>();
+    }
+    //CREAMOS HASHMAPS
     private Map<String, Persona> personaMapDni = new HashMap<>();
     private Map<String, Persona> personaMapNumeroSS = new HashMap<>();
     //A PARTIR DE AQUI HACER LOS METODOS CON HASH....
-    public void altaPersonaMap(Persona persona) {
+    public void altaPersona(Persona persona) {
         if (!personaMapDni.containsKey(persona.getDni()) && !personaMapNumeroSS.containsKey(persona.getNumSS())){
             personaMapDni.put(persona.getDni(), persona);
             personaMapNumeroSS.put(persona.getNumSS(), persona);
         }
     }
 
-    public void bajaPersonaMap(String dni){
+    public void bajaPersona(String dni){
         personaMapNumeroSS.remove(personaMapDni.get(dni).getNumSS());
         personasList.removeIf(persona -> persona.getDni().equals(dni));
     }
 
-    public Persona obtenerPersonaPorDNIMap(String dni){
+    public Persona obtenerPersonaPorDNI(String dni){
         return personaMapDni.get(dni);
     }
 
-    public Persona obtenerPersonaPorNumSSMap(String numSS){
+    public Persona obtenerPersonaPorNumSS(String numSS){
         return  personaMapNumeroSS.get(numSS);
     }
 
-    public List<Persona> obtenerPersonaRangoSalarialMap(double min, double max){
+    public List<Persona> obtenerPersonaRangoSalarial(double min, double max){
         return personaMapNumeroSS.values().stream().filter(persona -> persona.getSalario()>=min&&persona.getSalario()<max).collect(Collectors.toList());
     }
 
-    //////////////////////ARRAYLIST PART:
-    public SeguridadSocial() {
-        personasList = new ArrayList<>();
-    }
-
-    // Debes comprobar que no se introduzcan dos personas con el mismo DNI/Número Seguridad Social
-    public void altaPersona(Persona persona) {
-        personasList.add(persona);
-    }
-
-    public void bajaPersona(String dni) {
-        personasList.remove(obtenerPosDNI(dni));
-    }
-
-    public int obtenerPosDNI(String dni){
-        boolean enc = false;
-        int i = 0;
-        do {
-            Persona a =personasList.get(i);
-            if (a.getDni().equals(dni)) {
-                enc = true;
-            }
-            else{
-                i++;
-            }
-
-        }while (enc=false);
-
-        if (enc=false){
-            return -1;
-        }
-        else{
-            return i;
-        }
-    }
-
-    public Persona obtenerPersonaPorDNI(String dni) { return personasList.get(obtenerPosDNI(dni)); }
-
-    public Persona obtenerPersonaPorNumSS(String numSS) {
-            boolean enc = false;
-            int i = 0;
-            do {
-                Persona a = personasList.get(i);
-                if (a.getNumSS().equals(numSS)) {
-                    enc = true;
-                } else {
-                    i++;
-                }
-            } while (enc = false);
-            if (enc = false) {
-                return null;
-            } else {
-                return personasList.get(i);
-            }
-    }
-
-    /*public List<Persona> obtenerPersonasRangoSalarial(double min, double max){
-    }
-
     public List<Persona> obtenerPersonasMayoresQue(int edad){
+        return personaMapDni.values().stream().filter(persona -> persona.getEdad()>edad).collect(Collectors.toList());
     }
-    */
+
     public List<Persona> obtenerTodas(){
         return personasList;
     }
@@ -110,8 +55,4 @@ public class SeguridadSocial {
                 '}';
     }
 }
-
-/*HASHMAP PART
-    private Map<Persona> =
- */
 
